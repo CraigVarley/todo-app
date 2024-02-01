9ae432e7b5893ef572b318c602e75e6b06599505
Craig.Varley <varley.craig@gmail.com>
2024-02-01 15:49:44 -0500

    implement add button

--- gui.py
+++ gui.py
@@ -3,14 +3,24 @@ import PySimpleGUI as sg
 
 
 label = sg.Text("Type in a todo")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
 
 # window instance, Python doesn't have window instances by default, need 3rd party
-window = sg.Window('My App', layout=[[label, input_box, add_button]]) #layout expects a list of instances
-# *** inner square brackets are one row ***
+window = sg.Window('My App',
+                   layout=[[label, input_box, add_button]], # *** inner square brackets are one row ***
+                   font=('Helvetica', '10'))
+while True:
+    event, values = window.read() #button event and input value
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 
-window.read()
 window.close()
 
