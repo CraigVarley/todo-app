7b7d4665490667c5559c6fa48e3395eddcf47fce
Craig.Varley <varley.craig@gmail.com>
2024-02-05 11:48:27 -0500

    implement color, trys, time

--- functions.py
+++ functions.py
@@ -11,9 +11,11 @@ def write_todos(todos_arg, filepath='todos.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-# list the todos
-def enumerate_todos(todos):
-    for number, item in enumerate(todos):
+# list the todos from read file
+def enumerate_todos(filepath='todos.txt'):
+    with open(filepath, 'r') as file_local:  # context manager
+        todos_local = file_local.readlines()
+    for number, item in enumerate(todos_local):
         item = item.strip('\n')
         row = f"{number + 1}-{item}"
         print(row)
--- gui.py
+++ gui.py
@@ -1,24 +1,77 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme('Black')
 
+clock = sg.Text('', key="clock")
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+exit_button = sg.Button("Exit")
+complete_button = sg.Button("Complete")
+calendar = sg.CalendarButton("Choose A Complete By Time and Date", key="cal")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(45,10))
+edit_button = sg.Button("Edit")
 
 
 # window instance, Python doesn't have window instances by default, need 3rd party
 window = sg.Window('My App',
-                   layout=[[label, input_box, add_button]], # *** inner square brackets are one row ***
+                   layout=[[clock],
+                           [label, input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]], # *** inner square brackets are one row ***
                    font=('Helvetica', '10'))
 while True:
-    event, values = window.read() #button event and input value
+    event, values = window.read(timeout=10) #button event and input value
+    # TIMEOUT REQUIRD OTHERWISE THIS ONLY RUNS ON AN EVENT AND CLOCK NEEDS REFRESHING
+    window['clock'].update(value=time.strftime('%c'))
     match event:
         case "Add":
-            todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
+            todos_list = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            if new_todo == '\n':
+                sg.Popup('Write something you beautiful fool.')
+            else:
+                todos_list.append(new_todo)
+                functions.write_todos(todos_list)
+                window['todos'].update(values=todos_list)
+                window['todo'].update(value='')
+        case "Edit":
+            try:
+                todo_edit = values['todos'][0] # get event value
+                new_todo = values[('todo')] #get text box value
+
+                todos_list = functions.get_todos() #save todos list from txt file
+                todos_edit_index = todos_list.index(todo_edit) # get index of editing todo
+                todos_list[todos_edit_index] = new_todo #add new todo to same index
+                functions.write_todos(todos_list) # write to txt file
+                window['todos'].update(values=todos_list)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font=['Helvetica', 11])
+        case "todos":
+            window['todo'].update(value=values['todos'][0]) #all instances assigned to window
+        case "Exit":
+            sg.WIN_CLOSED
+            break
+        case "cal":
+            date = values['Calendar']
+            print(date)
+        case "Complete":
+            try:
+                # get selected todo
+                completed_todo = values['todo']
+                # delete from todos.txt
+                todos_list = functions.get_todos()
+                todos_list.remove(completed_todo)
+                functions.write_todos(todos_list)
+                window['todos'].update(values=todos_list)
+                window['todo'].update(value='')
+                # delete from list
+            except ValueError:
+                sg.popup("Please select an item first", font=['Helvetica', 11])
         case sg.WIN_CLOSED:
             break
 
